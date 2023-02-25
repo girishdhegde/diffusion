7b9c47b3ab93404645581bae3ce7ee5de4a5b0e4
Girish Hegde <girish.dhc@gmail.com>
2023-02-25 09:11:41 +0530

    training loop initial commit

--- ddpm/train.py
+++ ddpm/train.py
@@ -45,6 +45,7 @@ IMG_SIZE = 64
 BATCH_SIZE = 32
 GRAD_ACC_STEPS = 1  # used to simulate larger batch sizes
 MAX_ITERS = 100_000  # total number of training iterations
+EVAL_INTERVAL = 2000
 EVAL_ITERS = 100
 EVAL_ONLY = False  # if True, script exits right after the first eval
 SAVE_EVERY = False  # save unique checkpoint at every eval interval.
@@ -73,7 +74,7 @@ set_seed(108)
 torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
 torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
 torch.backends.cudnn.benchmark = True  # optimize backend algorithms
-extras = {}
+extras = {'epoch':1}
 
 # =============================================================
 # Dataset, Dataloader init
@@ -98,6 +99,8 @@ net = UNet(
     N_BLOCKS, GROUPS,
 )
 net_state, optim_state, itr, best, kwargs = load_checkpoint(LOAD)
+if 'epoch' in kwargs: extras['epoch'] = kwargs['epoch']
+epoch = extras['epoch']
 if net_state is not None:
     net.load_state_dict(net_state)
 net.to(DEVICE)
@@ -108,124 +111,91 @@ criterion = nn.MSELoss()
 print(f'Total model parameters = {net.n_params} = {net.n_params/1e6}M')
 
 # =============================================================
-# Learning Rate Decay Scheduler (cosine with warmup)
-# =============================================================
-def get_lr(iter):
-    """Refs:
-            https://github.com/karpathy/nanoGPT/blob/master/train.py
-    """
-    # 1) linear warmup for warmup_iters steps
-    if iter < WARMUP_ITERS:
-        return LR * iter / WARMUP_ITERS
-    # 2) if iter > lr_decay_iters, return min learning rate
-    if iter > LR_DECAY_ITERS:
-        return MIN_LR
-    # 3) in between, use cosine decay down to min learning rate
-    decay_ratio = (iter - WARMUP_ITERS) / (LR_DECAY_ITERS - WARMUP_ITERS)
-    assert 0 <= decay_ratio <= 1
-    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff ranges 0..1
-    return MIN_LR + coeff * (LR - MIN_LR)
-
-# =============================================================
 # Training loop - forward, backward, loss, optimize
 # =============================================================
-trainloss, valloss, log_trainloss = 0, 0, 0
-trainloader = iter(trainloader)
-net.train()
+trainloss, valloss, log_trainloss, loss_ = 0, 0, 0, 0
 optimizer.zero_grad(set_to_none=True)
-# set_to_none -> instead of filling grad with zero tensor set it to None
-# reduce memory consumptionn + increases speed
 print('Training ...')
 start_time = time.perf_counter()
-for itr in range(itr, MAX_ITERS + 1):
-    # =============================================================
-    # Validation
-    # =============================================================
-    if (itr%EVAL_INTERVAL == 0) or EVAL_ONLY:
-        print('Evaluating ...')
-        net.eval()
-        valloss = 0
-        with torch.no_grad():
-            for inp, tar in tqdm(evalloader, total=len(evalloader)):
-                inp, tar = inp.to(DEVICE), tar.to(DEVICE)
-                logits = net(inp)
-                loss = criterion(logits.reshape(-1, tokenizer.n_vocab), tar.reshape(-1))
-                valloss += loss.item()
-        net.train()
-
-        valloss = valloss/EVAL_ITERS
-        trainloss = trainloss/EVAL_INTERVAL
-
+while True:
+    for itr_, (x, t) in enumerate(trainloader):
         # =============================================================
-        # Saving and Logging
+        # Training
         # =============================================================
-        if EVAL_ONLY:
-            log_data = f'val loss: {valloss}, \t time: {(end_time - start_time)/60}M'
-            print(f'{"-"*150}\n{log_data}\n{"-"*150}')
-            break
-
-        print('Saving checkpoint ...')
-        ckpt_name = LOGDIR/'ckpt.pt' if not SAVE_EVERY else LOGDIR/f'ckpt_{itr}.pt'
-        save_checkpoint(
-            net, optimizer, itr, valloss, trainloss, best, ckpt_name, **extras,
-        )
-
-        if valloss < best:
-            best = valloss
-            save_checkpoint(
-                net, optimizer, itr, valloss, trainloss, best, LOGDIR/'best.pt', **extras,
-            )
-
-        write_pred(inp[0], logits[0], tokenizer, LOGDIR/'predictions.txt', label=f'iteration = {itr}')
-
-        logfile = LOGDIR/'log.txt'
-        log_data = f"iteration: {itr}/{MAX_ITERS}, \tval loss: {valloss}, \ttrain loss: {trainloss}, \tbest loss: {best}"
-        with open(logfile, 'a' if logfile.is_file() else 'w') as fp:
-            fp.write(log_data + '\n')
-        end_time = time.perf_counter()
-        log_data = f'{log_data}, \t time: {(end_time - start_time)/60}M'
-        print(f'{"-"*150}\n{log_data}\n{"-"*150}')
-
-        trainloss = 0
-        start_time = time.perf_counter()
-        print('Training ...')
-
-    # =============================================================
-    # Training
-    # =============================================================
-    # forward, loss, backward with grad. accumulation
-    loss_ = 0
-    for step in range(GRAD_ACC_STEPS):
-        inp, tar = next(trainloader)
-        inp, tar = inp.to(DEVICE), tar.to(DEVICE)
-
-        logits = net(inp)
-        loss = criterion(logits.reshape(-1, tokenizer.n_vocab), tar.reshape(-1))
+        # forward, loss, backward with grad. accumulation
+        x, t = x.to(DEVICE), t.to(DEVICE)
+        eps = net(x, t)
+        noise = None
+        loss = criterion(eps, noise)
         loss.backward()
-
         loss_ += loss.item()
 
-    # optimize params
-    loss_ = loss_/GRAD_ACC_STEPS
-    trainloss += loss_
-    log_trainloss += loss_
-    if GRADIENT_CLIP is not None:
-        nn.utils.clip_grad_norm_(net.parameters(), GRADIENT_CLIP)
+        # optimize params
+        if itr_%(GRAD_ACC_STEPS) == 0:
+            loss_ = loss_/GRAD_ACC_STEPS
+            trainloss += loss_
+            log_trainloss += loss_
+            loss_ = 0
+            if GRADIENT_CLIP is not None:
+                nn.utils.clip_grad_norm_(net.parameters(), GRADIENT_CLIP)
+            optimizer.step()
+            optimizer.zero_grad(set_to_none=True)
 
-    # cosine scheduler with warmup learning rate decay
-    if DECAY_LR:
-        lr = get_lr(itr)
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
-    optimizer.step()
-    optimizer.zero_grad(set_to_none=True)
+            # print info.
+            if itr%PRINT_INTERVAL == 0:
+                log_data = f"epoch: {epoch}, \titeration: {itr}, \ttrain loss: {log_trainloss/PRINT_INTERVAL}"
+                print(log_data)
+                log_trainloss = 0
 
-    # print info.
-    if itr%PRINT_INTERVAL == 0:
-        log_data = f"iteration: {itr}/{MAX_ITERS}, \ttrain loss: {log_trainloss/PRINT_INTERVAL}"
-        print(log_data)
-        log_trainloss = 0
+            # =============================================================
+            # Validation
+            # =============================================================
+            if (itr%EVAL_INTERVAL == 0):
+                print('Evaluating ...')
+                trainloss = trainloss/EVAL_INTERVAL
+                if evalloader is not None:
+                    net.eval()
+                    valloss = 0
+                    with torch.no_grad():
+                        for eitr, (x, t) in tqdm(enumerate(evalloader), total=len(evalloader)):
+                            x, t = x.to(DEVICE), t.to(DEVICE)
+                            eps = net(x, t)
+                            loss = criterion(eps, noise)
+                            valloss += loss.item()
+                            if eitr%EVAL_ITERS == 0: break
+                    net.train()
+                    valloss = valloss/EVAL_ITERS
+                else:
+                    valloss = trainloss
 
-# =============================================================
-# END
-# =============================================================
+                # =============================================================
+                # Saving and Logging
+                # =============================================================
+                print('Saving checkpoint ...')
+                ckpt_name = LOGDIR/'ckpt.pt' if not SAVE_EVERY else LOGDIR/f'ckpt_{itr}.pt'
+                extras['epoch'] = epoch
+                save_checkpoint(
+                    net, optimizer, itr, valloss, trainloss, best, ckpt_name, **extras,
+                )
+
+                if valloss < best:
+                    best = valloss
+                    save_checkpoint(
+                        net, optimizer, itr, valloss, trainloss, best, LOGDIR/'best.pt', **extras,
+                    )
+
+                # write_pred(inp[0], valid_logits, tokenizer, LOGDIR/'predictions.txt', label=f'iteration = {itr}')
+
+                logfile = LOGDIR/'log.txt'
+                log_data = f"epoch: {epoch}, \titeration: {itr}, \tval loss: {valloss}, \ttrain loss: {trainloss}, \tbest loss: {best}"
+                with open(logfile, 'a' if logfile.is_file() else 'w') as fp:
+                    fp.write(log_data + '\n')
+                end_time = time.perf_counter()
+                log_data = f'{log_data}, \t time: {(end_time - start_time)/60}M'
+                print(f'{"-"*150}\n{log_data}\n{"-"*150}')
+
+                trainloss = 0
+                start_time = time.perf_counter()
+                print('Training ...')
+            itr += 1
+    epoch += 1
