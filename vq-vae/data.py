1cc0252f911e0ac9c801a48a0b2f499bb8751a20
Girish Hegde <girish.dhc@gmail.com>
2023-05-01 19:36:26 +0530

    add cifar10 dataset and dataloader

---
+++ vq-vae/data.py
@@ -0,0 +1,41 @@
+# Ref: https://github.com/MishaLaskin/vqvae/blob/master/utils.py
+
+
+import numpy as np
+
+import torch
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+
+# https://github.com/MishaLaskin/vqvae/blob/master/utils.py
+def load_cifar():
+    train = datasets.CIFAR10(root="data", train=True, download=True,
+                             transform=transforms.Compose([
+                                 transforms.ToTensor(),
+                                 transforms.Normalize(
+                                     (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                             ]))
+
+    val = datasets.CIFAR10(root="data", train=False, download=True,
+                           transform=transforms.Compose([
+                               transforms.ToTensor(),
+                               transforms.Normalize(
+                                   (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                           ]))
+    return train, val
+
+
+# https://github.com/MishaLaskin/vqvae/blob/master/utils.py
+def data_loaders(train_data, val_data, batch_size):
+
+    train_loader = DataLoader(train_data,
+                              batch_size=batch_size,
+                              shuffle=True,
+                              pin_memory=True)
+    val_loader = DataLoader(val_data,
+                            batch_size=batch_size,
+                            shuffle=True,
+                            pin_memory=True)
+    return train_loader, val_loader
