28ce4994817d7613005bd789a73873e7bba6b685
Girish Hegde <girish.dhc@gmail.com>
2023-05-02 11:09:29 +0530

    add write_pred func

--- vq-vae/utils.py
+++ vq-vae/utils.py
@@ -48,7 +48,7 @@ def load_checkpoint(filename):
 
 
 @torch.no_grad()
-def write_pred(pred, outdir, name):
+def write_pred(pred, outdir, name, scale=1):
     outdir = Path(outdir)
     outdir.mkdir(exist_ok=True, parents=True)
     pred = pred.cpu().numpy().transpose(0, 2, 3, 1)
@@ -56,5 +56,10 @@ def write_pred(pred, outdir, name):
     pred = (np.clip(pred, 0, 1)*255).astype(np.uint8)
     for i, img in enumerate(pred):
         filename = outdir/f'{name}_{i}.png'
+        img = cv2.resize(
+            img, 
+            (int(img.shape[1]*scale), int(img.shape[0]*scale)), 
+            interpolation=cv2.INTER_CUBIC,
+        )
         cv2.imwrite(str(filename), img[..., ::-1])
     return
