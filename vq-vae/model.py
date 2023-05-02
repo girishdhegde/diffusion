069c7e02dc61e899debc13dd953c7e7f49261a3c
Girish Hegde <girish.dhc@gmail.com>
2023-05-02 10:07:11 +0530

    add load save funcs to vqvae

--- vq-vae/model.py
+++ vq-vae/model.py
@@ -92,17 +92,27 @@ class VectorQuantizer(nn.Module):
 
     
 class VQVAE(nn.Module):
-    def __init__(self, in_ch=3, res_layers=2, hidden_ch=256, num_emb=8*8*10, beta=0.25):
+    def __init__(
+        self, 
+        in_ch=3, res_layers=2, hidden_ch=256, num_emb=8*8*10, 
+        beta=0.25, lr=2e-4, 
+        ckpt=None, inference=False,
+    ):
         super().__init__()
         self.in_ch = in_ch
         self.res_layers = res_layers
         self.hidden_ch = hidden_ch
         self.num_emb = num_emb
         self.beta = beta
+        self.lr = lr
 
-        self.enc = Encoder(in_ch, res_layers, hidden_ch)
-        self.vq  = VectorQuantizer(num_emb, hidden_ch, beta)
-        self.dec = Decoder(in_ch, res_layers, hidden_ch)
+        if ckpt is None:
+            self.enc = Encoder(in_ch, res_layers, hidden_ch)
+            self.vq  = VectorQuantizer(num_emb, hidden_ch, beta)
+            self.dec = Decoder(in_ch, res_layers, hidden_ch)
+            self.opt = torch.optim.Adam(self.parameters(), lr=lr)
+        else:
+            self.load_ckpt(ckpt, inference)
 
     def forward(self, x):
         ze = self.enc(x)
@@ -112,3 +122,59 @@ class VQVAE(nn.Module):
         recon_loss = F.mse_loss(x, x_)
         loss = recon_loss + emb_loss
         return (ze, z, zq, x_), loss
+
+    def get_config(self):
+        cfg = {
+            'in_ch':self.in_ch, 'res_layers':self.res_layers, 'hidden_ch':self.hidden_ch, 'num_emb':self.num_emb, 
+            'beta':self.beta, 'lr':self.lr, 
+        }
+        return cfg
+
+    def get_ckpt(self):
+        ckpt = {
+            'net':{
+                'config': self.get_config(),
+                'encoder': self.enc.state_dict(),
+                'vector_quantizer': self.vq.state_dict(),
+                'decoder': self.dec.state_dict(),
+            },
+            'optimizer': self.opt.state_dict(),
+        }
+        return ckpt
+
+    def load_ckpt(self, ckpt, inference=False):
+        if not isinstance(ckpt, dict): ckpt = torch.load(ckpt, map_location='cpu')
+        config, enc, vq, dec = ckpt['net']
+        for k, v in config.items():
+            setattr(self, k, v)
+        self.enc = Encoder(self.in_ch, self.res_layers, self.hidden_ch)
+        self.vq  = VectorQuantizer(self.num_emb, self.hidden_ch, self.beta)
+        self.dec = Decoder(self.in_ch, self.res_layers, self.hidden_ch)
+        self.enc.load_state_dict(enc)
+        self.vq.load_state_dict(vq)
+        self.dec.load_state_dict(dec)
+        self.to(self.device)
+        print(f'Model loaded successfully ...')
+
+        if (not inference) and ('optimizer' in ckpt):
+            self.opt = torch.optim.Adam(self.parameters(), lr=1e-1)
+            self.opt.load_state_dict(ckpt['optimizer'])
+            print(f'Optimizer loaded successfully ...')
+
+    def save_ckpt(self, filename):
+        ckpt = self.get_ckpt()
+        torch.save(ckpt, filename)
+    
+    def zero_grad(self, *args, **kwargs):
+        self.opt.zero_grad(*args, **kwargs)
+
+    def optimize(self, gradient_clip=None, new_lr=None, *args, **kwargs):
+        if gradient_clip is not None:
+            nn.utils.clip_grad_norm_(self.opt.parameters(), gradient_clip)
+
+        if new_lr is not None:
+            for param_group in self.opt.param_groups:
+                param_group['lr'] = new_lr
+
+        self.opt.step()
+        self.zero_grad(*args, **kwargs)
